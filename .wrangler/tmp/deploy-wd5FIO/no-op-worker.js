8d29f5f5b2eff2a4846be6135a992b266ea87d84
masaya48 <msya0408@gmail.com>
2025-12-30 16:47:37 +0900

    微修正

---
+++ .wrangler/tmp/deploy-wd5FIO/no-op-worker.js
@@ -0,0 +1,15 @@
+// ../../../Library/Caches/pnpm/dlx/18cf3e20b05f6834de1ebf4598461ef2bf0fee86f11c39ce8b27b551ccb00111/19b6988aaaa-ebd9/node_modules/.pnpm/wrangler@4.54.0/node_modules/wrangler/templates/no-op-worker.js
+var no_op_worker_default = {
+  fetch() {
+    return new Response("Not found", {
+      status: 404,
+      headers: {
+        "Content-Type": "text/html"
+      }
+    });
+  }
+};
+export {
+  no_op_worker_default as default
+};
+//# sourceMappingURL=no-op-worker.js.map
